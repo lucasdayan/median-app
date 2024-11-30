712cd0d700678db86dbbb77f653ad3ed0a543adf
lucasdayan <lucas.dayan4@gmail.com>
2024-11-30 13:58:48 -0700

    deleting .env

--- nestjs/src/user/user.service.ts
+++ nestjs/src/user/user.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: any) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+}
